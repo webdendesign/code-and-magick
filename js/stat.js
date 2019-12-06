c5eed9d029a0e6282a4153ab180f0b0db040ac4e
Den <fabri.din@yandex.ru>
2019-12-06 19:53:41 +0500

    gistogramma

--- js/stat.js
+++ js/stat.js
@@ -1,15 +1,21 @@
-window.renderStatistics = function (ctx, names, times) {
+"use strict";
 
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+
+var renderCloud = function(ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+window.renderStatistics = function(ctx) {
   var positionStartX = 100;
   var positionStartY = 10;
 
-  //shadow
-  ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
-  ctx.fillRect(positionStartX + 10, positionStartY + 10, 420, 270);
-
-  //cluod
-  ctx.fillStyle = "#ffffff";
-  ctx.fillRect(positionStartX, positionStartY, 420, 270);
+  renderCloud(ctx, 110, 20, "rgba(0, 0, 0, 0.7)");
+  renderCloud(ctx, 100, 10, "#fff");
 
   //text
   ctx.fillStyle = "#000000";
@@ -21,20 +27,30 @@ window.renderStatistics = function (ctx, names, times) {
   var colomnWidth = 40;
   var step = colomnWidth + 50;
 
-  for (var i = 0; i < times.lenght; i++) {
-    var name = names[i];
-    var time = times[i];
+  ctx.fillStyle = "rgba(255, 0, 0, 1)";
+  ctx.fillText("Вы", 140, 280);
+  ctx.fillRect(140, 240, 40, -50);
 
+  ctx.fillStyle = "blue";
+  ctx.fillText("Иван", 230, 260);
+  ctx.fillRect(230, 90, 430, 20);
 
+  ctx.fillStyle = "grey";
+  ctx.fillText("Юлия", 320, 260);
+  ctx.fillRect(320, 120, 430, 20);
 
-    if (name === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      ctx.filter = 'none';
-    } else {
-      ctx.fillStyle = 'blue';
-      ctx.filter = 'saturate(' + Math.random() + ')';
-    }
+  //   for (var i = 0; i < times.lenght; i++) {
+  //     var name = names[i];
+  //     var time = times[i];
 
-    ctx.fillRect( , , 40, )
-  }
-}
+  //     if (name === 'Вы') {
+  //       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+  //       ctx.filter = 'none';
+  //     } else {
+  //       ctx.fillStyle = 'blue';
+  //       ctx.filter = 'saturate(' + Math.random() + ')';
+  //     }
+
+  //     ctx.fillRect( , , 40, )
+  //   }
+};
